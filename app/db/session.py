be7a20e1d66dba43572ee94120cbc37a70a87a7f
gk <iba3374@naver.com>
2025-04-15 23:34:54 +0900

    fix: db설정 변경

--- app/db/session.py
+++ app/db/session.py
@@ -2,8 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-# DATABASE_URL = f"mysql+aiomysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
-DATABASE_URL = "sqlite+aiosqlite:///:memory:"
+DATABASE_URL = settings.DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 
